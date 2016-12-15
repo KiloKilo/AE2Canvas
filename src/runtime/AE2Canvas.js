65a4653b693d48aa66d4beb84124dc177377c734
Luc <lukas.zgraggen@kilokilo.ch>
2016-12-15 17:27:31 +0100

    cleanup

--- src/runtime/AE2Canvas.js
+++ src/runtime/AE2Canvas.js
@@ -6,31 +6,6 @@ var Group = require('./Group'),
 var _animations = [],
     _animationsLength = 0;
 
-// @license http://opensource.org/licenses/MIT
-// copyright Paul Irish 2015
-// (function () {
-//
-//     if ('performance' in window == false) {
-//         window.performance = {};
-//     }
-//
-//     if ('now' in window.performance == false) {
-//
-//         var nowOffset = Date.now();
-//
-//         if (performance.timing && performance.timing.navigationStart) {
-//             nowOffset = performance.timing.navigationStart
-//         }
-//
-//         window.performance.now = function now() {
-//             return Date.now() - nowOffset;
-//         }
-//     }
-//
-//     //
-//
-// })();
-
 function Animation(options) {
     if (!options.data) {
         console.error('no data');
