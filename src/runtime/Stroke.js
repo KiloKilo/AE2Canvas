a4f0c763e907ed14c1bfcfc8499aad601c049f55
Luc <lukas.zgraggen@kilokilo.ch>
2017-05-19 16:46:07 +0200

    add dashed stroke

--- src/runtime/Stroke.js
+++ src/runtime/Stroke.js
@@ -21,6 +21,19 @@ function Stroke(data) {
 
         if (data.width.length > 1) this.width = new AnimatedProperty(data.width);
         else this.width = new Property(data.width);
+
+        if (data.dashes) {
+            this.dashes = {};
+
+            if (data.dashes.dash.length > 1) this.dashes.dash = new AnimatedProperty(data.dashes.dash);
+            else this.dashes.dash = new Property(data.dashes.dash);
+
+            if (data.dashes.gap.length > 1) this.dashes.gap = new AnimatedProperty(data.dashes.gap);
+            else this.dashes.gap = new Property(data.dashes.gap);
+
+            if (data.dashes.offset.length > 1) this.dashes.offset = new AnimatedProperty(data.dashes.offset);
+            else this.dashes.offset = new Property(data.dashes.offset);
+        }
     }
 }
 
@@ -46,6 +59,14 @@ Stroke.prototype.setStroke = function (ctx, time) {
     if (miterLimit) ctx.miterLimit = miterLimit;
     ctx.lineCap = this.cap;
     ctx.strokeStyle = strokeColor;
+
+    if (this.dashes) {
+        ctx.setLineDash([
+            this.dashes.dash.getValue(time),
+            this.dashes.gap.getValue(time)
+        ]);
+        ctx.lineDashOffset = this.dashes.offset.getValue(time);
+    }
 };
 
 Stroke.prototype.setKeyframes = function (time) {
@@ -53,6 +74,11 @@ Stroke.prototype.setKeyframes = function (time) {
     this.opacity.setKeyframes(time);
     this.width.setKeyframes(time);
     if (this.miterLimit) this.miterLimit.setKeyframes(time);
+    if (this.dashes) {
+        this.dashes.dash.setKeyframes(time);
+        this.dashes.gap.setKeyframes(time);
+        this.dashes.offset.setKeyframes(time);
+    }
 };
 
 Stroke.prototype.reset = function (reversed) {
@@ -60,6 +86,11 @@ Stroke.prototype.reset = function (reversed) {
     this.opacity.reset(reversed);
     this.width.reset(reversed);
     if (this.miterLimit) this.miterLimit.reset(reversed);
+    if (this.dashes) {
+        this.dashes.dash.reset(reversed);
+        this.dashes.gap.reset(reversed);
+        this.dashes.offset.reset(reversed);
+    }
 };
 
 module.exports = Stroke;
