c166f9b981aa52ece59950eadf9b4c41091ace04
Lukas Zgraggen <lukas.zgraggen@kilokilo.ch>
2019-02-27 11:26:37 +0100

    update dependencies and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,21 @@ var webpack = require('webpack');
 var path = require('path');
 
 var config = {
-    entry: __dirname + '/src/index.js',
-    devtool: 'source-map',
+    entry: path.join(__dirname, 'src/index.js'),
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname, 'dist'),
         filename: 'index.js',
         library: 'AE2Canvas',
         libraryTarget: 'umd'
     },
     module: {
-        loaders: [
+        rules: [
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
     },
     devServer: {
         contentBase: path.join(__dirname, 'examples'),
-        publicPath: '/',
+        publicPath: '/public',
         stats: 'errors-only',
         port: 8000,
         host: '0.0.0.0',
