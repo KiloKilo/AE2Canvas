b907a4154d4a71ebbb7901c8bae321f6b742158e
Lukas Zgraggen <lukas.zgraggen@kilokilo.ch>
2020-05-29 13:28:51 +0200

    add support for blendmodes

--- src/layers/BaseLayer.js
+++ src/layers/BaseLayer.js
@@ -1,41 +1,45 @@
-import Path from '../objects/Path'
-import AnimatedPath from '../objects/AnimatedPath'
-import Transform from '../transform/Transform'
-import DropShadow from '../effects/DropShadow'
+import Path from "../objects/Path";
+import AnimatedPath from "../objects/AnimatedPath";
+import Transform from "../transform/Transform";
+import DropShadow from "../effects/DropShadow";
+import BlendingMode from "../property/BlendingMode";
 
 class BaseLayer {
 	constructor(data) {
-		this.index = data.index
-		this.in = data.in || 0
-		this.out = data.out
-		if (data.parent) this.parent = data.parent
-		this.transform = new Transform(data.transform)
+		this.index = data.index;
+		this.in = data.in || 0;
+		this.out = data.out;
+		if (data.parent) this.parent = data.parent;
+		if (data.blendMode) this.blendMode = new BlendingMode(data.blendMode);
+		this.transform = new Transform(data.transform);
 
 		if (data.effects) {
 			if (data.effects.dropShadow) {
-				this.dropShadow = new DropShadow(data.effects.dropShadow)
+				this.dropShadow = new DropShadow(data.effects.dropShadow);
 			}
 		}
 
 		if (data.masks) {
-			this.masks = data.masks.map(mask => (mask.isAnimated ? new AnimatedPath(mask) : new Path(mask)))
+			this.masks = data.masks.map(mask => (mask.isAnimated ? new AnimatedPath(mask) : new Path(mask)));
 		}
 	}
 
 	draw(ctx, time) {
-		ctx.save()
+		ctx.save();
 
-		if (this.parent) this.parent.setParentTransform(ctx, time)
-		this.transform.update(ctx, time)
+		if (this.parent) this.parent.setParentTransform(ctx, time);
+		if (this.blendMode) this.blendMode.setCompositeOperation(ctx);
+
+		this.transform.update(ctx, time);
 
 		if (this.dropShadow) {
-			this.dropShadow.setShadow(ctx, time)
+			this.dropShadow.setShadow(ctx, time);
 		}
 
 		if (this.masks) {
-			ctx.beginPath()
-			this.masks.forEach(mask => mask.draw(ctx, time))
-			ctx.clip()
+			ctx.beginPath();
+			this.masks.forEach(mask => mask.draw(ctx, time));
+			ctx.clip();
 		}
 	}
 
--- src/property/BlendingMode.js
+++ src/property/BlendingMode.js
@@ -1,25 +1,10 @@
 class BlendingMode {
-	constructor(data) {
-		this.type = data.type
+	constructor(type) {
+		this.type = type.toLowerCase().replace("_", "-");
 	}
 
 	setCompositeOperation(ctx) {
-		switch (this.type) {
-			case 2:
-				ctx.globalCompositeOperation = 'source-over'
-				break
-			case 3:
-				ctx.globalCompositeOperation = 'source-out'
-				break
-			case 4:
-				ctx.globalCompositeOperation = 'source-in'
-				break
-			case 5:
-				ctx.globalCompositeOperation = 'xor'
-				break
-			default:
-				ctx.globalCompositeOperation = 'source-over'
-		}
+		ctx.globalCompositeOperation = this.type;
 	}
 }
 
