300840851390d0d7134f347ab24325768be8cf12
Lukas Zgraggen <lukas.zgraggen@kilokilo.ch>
2023-05-23 10:07:06 +0200

    hotfix scale in nulllayer

--- src/layers/NullLayer.ts
+++ src/layers/NullLayer.ts
@@ -1,8 +1,8 @@
 import BaseLayer from './BaseLayer'
 
 class NullLayer extends BaseLayer {
-	draw(ctx: CanvasRenderingContext2D, time: number) {
-		super.draw(ctx, time)
+	draw(ctx: CanvasRenderingContext2D, time: number, scale: number) {
+		super.draw(ctx, time, scale)
 		ctx.restore()
 	}
 }
